7019cb855dfa8b3549d5097a572a7fa58b666bfa
Wayne Tsui <tsuiwwwayne@hotmail.com>
2016-01-22 12:08:32 +0000

    added task1_2 files

---
+++ task1_2.c
@@ -0,0 +1,13 @@
+/*
+  Created by Matthew Bell and Wayne Tsui. 
+*/
+
+#include "simpletools.h"                      
+#include "abdrive.h"
+#include "librobot.h"
+
+int main() {
+  while(1) {
+    turn_pivot_function(360);  
+  }  
+}
