f14c8752868bbbb64d8f7c14a98eb17ef4ea449e
Matthew Bell <logicnotlogical@outlook.com>
2016-01-28 22:56:40 +0000

    Improved the bubble sort algorithm. I also abstracted the printing and swapping to new functions. This means that task 2 is essentially done already.

--- task2_1.c
+++ task2_1.c
@@ -1,44 +1,65 @@
 /*
-  Created by Matthew Bell and Wayne Tsui.
-  This programme sorts a user input list of random numbers in ascending order using bubble sort algorithim. 
-*/
-
+   Created by Matthew Bell and Wayne Tsui.
+   This programme sorts a user input list of random numbers in ascending order using bubble sort algorithim. 
+   */
+#include <stdbool.h>
 #include "simpletools.h"                     
 
-  int* getIntArr() {
-    int i;
-    printf("Enter number of elements\n");
-    scanf("%i", &i);
-    int *inputs = malloc(sizeof(int) * i);
-    printf("Enter %i integers\n", i);
-    
-    for (int n = 0; n < i; n++) {
-      scanf("%i", (inputs+n));
-      printf("%i", *(inputs+n));
-    }      
-    return *inputs;
+#define _ARR_SIZE 5
+
+int* getIntArr() {
+  int i;
+  printf("Enter number of elements\n");
+  scanf("%i", &i);
+  int *inputs = malloc(sizeof(int) * i);
+  printf("Enter %i integers\n", i);
+
+  for (int n = 0; n < i; n++) {
+    scanf("%i", (inputs+n));
+    printf("%i", *(inputs+n));
+  }      
+  return *inputs;
+}
+
+// swaps two integers.
+void swap(int*a, int*b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+// prints the given array to stdout.
+void print_array(int *arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    printf("%i ",arr[i]);
   }
-  
-  void bubbleSort(int* g) {
-    int temp;
-    
-    for (int i = 0; i < 5; i++) {
-      if (*(g+i) > *(g+i+1)) {
-        temp = *(g+i+1);
-        *(g+i+1) = *(g+i);
-        *(g+i) = temp;
-      }        
+  printf("\n");
+}
+
+
+void bubble_sort(int* arr, int length) {
+  bool swapped;
+  int n = length;
+  int i;
+  do {
+    swapped = false;
+    for (i = 1; i < n; i++) {
+      if ( arr[i-1] > arr[i] ) {
+        swap(&arr[i-1],&arr[i]);
+        swapped=true;
+      }
     }
-    
-    for (int n = 0; n < 5; n++) {
-      printf("%i\n", *(g+n));
-    }            
-  }       
+    n--;
+
+  } while (swapped);
+  print_array(arr,length);
+}
 
 int main() {
-    //int* a = getIntArr();
-    //bubbleSort(getIntArr());
-    //free(a);
-                    
+  //int* a = getIntArr();
+  //bubble_sort(getIntArr(),_ARR_SIZE);
+  //free(a);
+
 }
-  
+
