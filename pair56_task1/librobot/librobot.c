58cbd1b512557b2e21b929539f3942fe94c19b2a
Wayne Tsui <tsuiwwwayne@hotmail.com>
2016-01-28 11:10:08 +0000

    Added files for submission

---
+++ pair56_task1/librobot/librobot.c
@@ -0,0 +1,12 @@
+/*
+  Created by Matthew Bell and Wayne Tsui.
+*/
+
+#include "simpletools.h"
+#include "robot.h"
+
+int main() {
+  while(1) {
+    turn_pivot_function(360);
+  }
+}
---
+++ pair56_task1/task1_1.c
@@ -0,0 +1,22 @@
+/*
+  Created by Matthew Bell and Wayne Tsui.
+  This programme gets the robot to move in a 1-metre square.
+*/
+
+#include "simpletools.h"                      
+#include "abdrive.h"
+#include "robot.h"
+
+void drawSquare(int n) {
+  for(int i = 0; i < 4; i++) {
+  drive_goto(n/3.25,n/3.25);
+  turn_pivot_function(90);
+  }  
+}
+
+int main()                                    
+{
+  
+  drawSquare(1000);
+ 
+}
