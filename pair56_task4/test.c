a9fe057634aa915c15a30bbb91bc36319aa83e35
Matthew Bell <logicnotlogical@outlook.com>
2016-02-25 10:46:42 +0000

    updated makefiles

---
+++ pair56_task4/test.c
@@ -0,0 +1,20 @@
+#include "simpletools.h"
+#include "abdrive.h"
+#include "ping.h"
+#include "librobot.h"
+
+#define _PROPELLER_IO_PIN 8
+
+int distance = 20;
+
+int main() {
+    while(distance > 10)
+    {
+        drive_goto(50,50);
+        distance = ping_cm(8);
+        print("%c distance = %d%c cm",
+                HOME, distance, CLREOL);
+        pause(200);
+    }
+    drive_goto(0,0);
+}
